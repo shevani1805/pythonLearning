b51abda8b5c58f1ba4b2f34261a1230ac66c9821
shevani1805 <prshevani@gmail.com>
2024-04-16 23:53:45 +0530

    revition 2

---
+++ TYPEOFCLASS AND VARIBLEEX1.py
@@ -0,0 +1,18 @@
+class phone():
+    def __init__(self,brand,price,chargertype):
+        self.brand=brand =brand
+        self.price=price
+        self.chargertype=chargertype
+    def display(self.brand)
+        print("Brand:",self.brand)
+        print("price:",self.price)
+        print("chargertype",self.chargertype)
+
+samsung=phone("samsung","10000","B-TYPE") 
+ samsung.display()
+    
+redmi=phone("Redmi","5000")
+redmi.display()
+
+google=phone("pixel","5000")
+redmi.display()        
